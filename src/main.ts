b71177cefa2dfea6b91ae8af4070c13d65336315
thanghh12 <thang.hh194837@sis.hust.edu.vn>
2024-10-18 22:36:40 +0700

    iniital project

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { ValidationPipe } from "@nestjs/common";
+
+async function bootstrap() {
+  const app = await NestFactory.create();
+  app.setGlobalPrefix("api");
+
+  const config = new DocumentBuilder()
+    .setTitle("Web chat API")
+    .setDescription("API description for web chat application")
+    .setVersion("1.0")
+    .addTag("Chat")
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("api", app, document);
+
+  app.enableCors({
+    origin: ["http://localhost:5173"],
+  });
+  app.useGlobalPipes(new ValidationPipe());
+  await app.listen(3000);
+}
+bootstrap();
