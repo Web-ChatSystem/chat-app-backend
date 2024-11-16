d14d300be87c97b7563ab46901ff3250584f432d
thanghh12 <thang.hh194837@sis.hust.edu.vn>
2024-11-16 22:45:26 +0700

    apply new module

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,9 @@ import {
   UserModule,
   ParticipantModule,
   ConversationModule,
+  MessageModule,
+  MessageStatusModule,
+  NicknameModule
 } from "./modules";
 @Module({
   imports: [
@@ -16,6 +19,9 @@ import {
     RelationshipModule,
     RequestModule,
     ConversationModule,
+    MessageModule,
+    MessageStatusModule,
+    NicknameModule
   ],
   controllers: [],
   providers: [PrismaService],
