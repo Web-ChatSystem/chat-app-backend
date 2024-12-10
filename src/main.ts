a99c1eca731d4c1a8a69c17207ab0540c793ca2f
SonProCode <sonchl20182021@gmail.com>
2024-12-11 03:28:55 +0700

    them https

--- src/main.ts
+++ src/main.ts
@@ -20,7 +20,7 @@ async function bootstrap() {
   SwaggerModule.setup("api", app, document);
 
   app.enableCors({
-    origin: ["http://localhost:4173", "https://chat-app-frontend-v2-9xdq.vercel.app", "https://chat-app-frontend-v2.onrender.com", "https://chat-app-frontend-myn0.onrender.com", "www.hust-cv-thson-20210744-web-app.id.vn"],
+    origin: ["http://localhost:4173", "https://chat-app-frontend-v2-9xdq.vercel.app", "https://chat-app-frontend-v2.onrender.com", "https://chat-app-frontend-myn0.onrender.com", "https://www.hust-cv-thson-20210744-web-app.id.vn"],
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe());
